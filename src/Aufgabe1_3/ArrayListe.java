7d721cc6b6d5e9b2f562b280f17fe78aa0ffb912
Manuel Scholz <m.scholz@hawksracing.de>
2016-10-18 13:40:15 +0200

    Added Class ArrayListe<T>

---
+++ src/Aufgabe1_3/ArrayListe.java
@@ -0,0 +1,83 @@
+/**
+ * 
+ */
+package Aufgabe1_3;
+
+/**
+ * @author Manuel Scholz
+ *
+ */
+public class ArrayListe<T> {
+  
+  int anzahlElemente;
+  private Object[] elemente;
+
+  /**
+   * 
+   */
+  public ArrayListe() {
+    // TODO Auto-generated constructor stub
+  }
+  /**
+   * 
+   * @param object
+   */
+  public void hinzufuegen(T object)
+  {
+    
+  }
+  /**
+   * 
+   * @param i
+   * @return
+   */
+  public T get(int i)
+  {
+    return null;
+  }
+  /**
+   * 
+   * @param Object
+   */
+  public void entfernen(T Object)
+  {
+    
+  }
+  /**
+   * 
+   * @param i
+   */
+  public void entferneElementAnIndex(int i)
+  {
+    
+  }
+  /**
+   * 
+   * @return
+   */
+  public int getanzahlElemente()
+  {
+    return 0;
+    
+  }
+  
+  /**
+   * 
+   * @return
+   */
+  @Override
+  public String toString()
+  {
+    return "";
+  }
+  
+  /**
+   * 
+   * @return
+   */
+  public T getKleinstesElement()
+  {
+    return null;
+  }
+
+}
