4fdc3296996430411d5442c38905d47e81a6186f
Manuel Scholz <m.scholz@hawksracing.de>
2016-10-25 12:04:18 +0200

    Vorlesungsfolien hinzugefuegt, Aufgabe 2 begonnen

---
+++ src/Aufgabe2/Rechner.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Aufgabe2;
+
+/**
+ * @author Manuel Scholz
+ *
+ */
+public class Rechner {
+
+  enum Operation{
+    addiere,
+    subtrahiere,
+    multipliziere,
+    dividiere
+  }
+  /**
+   * 
+   */
+  public Rechner() {
+    // TODO Auto-generated constructor stub
+  }
+  
+  public double berechne(Operation o, double w1, double w2)
+  {
+    
+    return 0;
+  }
+
+}
