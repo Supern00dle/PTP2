95d253e894f575980697414fe102248735a0f877
Manuel Scholz <m.scholz@hawksracing.de>
2016-11-10 16:38:19 +0100

    Comments, Tabs und einmal private

--- src/Aufgabe2_3/Flughafen.java
+++ src/Aufgabe2_3/Flughafen.java
@@ -12,85 +12,87 @@ import java.util.List;
 import Aufgabe2_3.Flughafen;
 import Aufgabe2_3.Flugzeug;
 
-public class Flughafen extends Thread
-{
-	private int maxAnzahlFlugzeuge;
-	private List<Flugzeug> flugzeuge;
-	private int index;
+public class Flughafen extends Thread {
+  private int maxAnzahlFlugzeuge;
+  private List<Flugzeug> flugzeuge;
+  private int index;
 
-	public Flughafen(int maxAnzhalFlugzeuge)
-	{
-		this.maxAnzahlFlugzeuge = maxAnzhalFlugzeuge;
-		flugzeuge = new ArrayList<Flugzeug>();
-		index = 0;
-	}
+  public Flughafen(int maxAnzhalFlugzeuge) {
+    this.maxAnzahlFlugzeuge = maxAnzhalFlugzeuge;
+    flugzeuge = new ArrayList<Flugzeug>();
+    index = 0;
+  }
 
-	@Override
-	public void run() 
-	{
-		while (!isInterrupted())
-		{
-			if (flugzeuge.size() < maxAnzahlFlugzeuge)
-			{
-				Flugzeug flugzeug = erzeugeFlugzeug(
-						(int) (10000 * Math.random()), "Flugzeug " + index);
-				flugzeug.start();
-				flugzeuge.add(flugzeug);
-			}
-			for (int i = 0; i < flugzeuge.size(); i++)
-			{
-				Flugzeug flug = flugzeuge.get(i);
-				flug.setZeit((int) (LocalTime.now().toNanoOfDay() / 1000000));
+  @Override
+  /**
+   * Die Run() Funktion des Flughafens. Zum starteten eines neuen Thread start()
+   * aufrufen!
+   */
+  public void run() {
+    while (!isInterrupted()) {
+      if (flugzeuge.size() < maxAnzahlFlugzeuge) {
+        Flugzeug flugzeug = erzeugeFlugzeug((int) (10000 * Math.random()), "Flugzeug " + index);
+        flugzeug.start();
+        flugzeuge.add(flugzeug);
+      }
+      for (int i = 0; i < flugzeuge.size(); i++) {
+        Flugzeug flug = flugzeuge.get(i);
+        flug.setZeit((int) (LocalTime.now().toNanoOfDay() / 1000000));
 
-				if (flug.istGelandet())
-				{
-					flugzeuge.remove(i);
-					flug.interrupt();
-					System.out.println(flug.toString() + "\n");
-				}
-				else
-				{
-					System.out.println(flug.toString() + "\n\n");
-				}
-			}
+        if (flug.istGelandet()) {
+          flugzeuge.remove(i);
+          flug.interrupt();
+          System.out.println(flug.toString() + "\n");
+        } else {
+          System.out.println(flug.toString() + "\n\n");
+        }
+      }
 
-			try
-			{
-				Thread.sleep(500);
-			}
-			catch (InterruptedException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
-	private Flugzeug erzeugeFlugzeug(int flugdauer, String id)
-	{
-		index++;
-		return new Flugzeug(id, flugdauer, this,
-				(int) (LocalTime.now().toNanoOfDay() / 1000000));
-	}
+  /**
+   * Erzeugt ein Flugzeug und fuegt es direkt zum Flughafen hinzu.
+   * 
+   * @param flugdauer
+   *          Die Dauer des Fluges
+   * @param id
+   *          Die Flugzeug ID
+   * @return Das fertige Flugzeug.
+   */
+  private Flugzeug erzeugeFlugzeug(int flugdauer, String id) {
+    index++;
+    return new Flugzeug(id, flugdauer, this, (int) (LocalTime.now().toNanoOfDay() / 1000000));
+  }
 
-	public synchronized void landen(Flugzeug flugzeug)
-	{
-		try
-		{
-			Thread.sleep(1500);
-		}
-		catch (InterruptedException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		flugzeug.gelandet();
-	}
+  /**
+   * Funktion zum landen lassen des Flugzeugs. Synchronized, also kann nur von
+   * einem Thread gleichzeitig aufgerufen werden.
+   * 
+   * @param flugzeug
+   */
+  public synchronized void landen(Flugzeug flugzeug) {
+    try {
+      Thread.sleep(1500);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    flugzeug.gelandet();
+  }
 
-	public static void main(String[] args)
-	{
-		Flughafen hafen = new Flughafen(5);
-		hafen.start();
+  /**
+   * Die Main Funktion des Programms.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    Flughafen hafen = new Flughafen(5);
+    hafen.start();
 
-	}
+  }
 }
